0107b559b2cc8e30b3e8e90ff891936f9f872fb3
Keith Galli <k.galli603@gmail.com>
2020-10-25 13:43:00 -0400

    updated analysis

--- analysis_cleaner.py
+++ analysis_cleaner.py
@@ -1,44 +1,53 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def retrieve_state_data(states=['Massachusetts'], days=7, mode='cases'):
-	if mode=='Cases':
-		df = load_USA_time_series_data().groupby('Province_State').sum().reset_index()
-	else:
-		df = load_USA_deaths().groupby('Province_State').sum().reset_index()
+CASES = 'Cases'
+DEATHS = 'Deaths'
 
-	for state in states:
-		state_data = df[df['Province_State'] == state]
-		state_time_series = state_data.diff(axis=1)
+def plot_states(states=['Massachusetts'], days=7, mode=CASES, filename=None):
+	COLUMN = 'Province_State'
+	df = load_relevant_data(True, mode).groupby(COLUMN).sum().reset_index()
+	plot_data(df, states, days, mode, COLUMN, filename)
 
-		x_values = list(state_time_series.columns[-days:])
-		y_values = [int(state_time_series[col]) for col in x_values]
+def plot_countries(countries=['US'], days=7, mode=CASES, filename=None):
+	COLUMN = 'Country/Region'
+	df = load_relevant_data(False, mode).groupby(COLUMN).sum().reset_index()
+	plot_data(df, countries, days, mode, COLUMN, filename)
 
-		plt.plot(x_values, y_values, label=state)
+def load_relevant_data(us_data=True, mode=CASES):
+	# This can be changed to your local directory (./) for testing purposes
+	#BASE_PATH = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
+	BASE_PATH = './'
+	if us_data and mode == CASES:
+		PATH = BASE_PATH + 'time_series_covid19_confirmed_US.csv'
+	elif us_data and mode == DEATHS:
+		PATH = BASE_PATH + 'time_series_covid19_deaths_US.csv'
+	elif not us_data and mode == CASES:
+		PATH = BASE_PATH + 'time_series_covid19_confirmed_global.csv'
+	elif not us_data and mode == DEATHS:
+		PATH = BASE_PATH + 'time_series_covid19_deaths_global.csv'
 
-	graph_time_series_data(x_values, y_values, state, mode)
+	return pd.read_csv(PATH)
 
-def graph_time_series_data(x_values, y_values, state, mode):
+def plot_data(df, places, days, mode, column, filename):
+	for place in places:
+		cumulative_data = df[df[column] == place]
+		counts = cumulative_data.diff(axis=1)
+		x_values = list(counts.columns[-days:])
+		y_values = [int(counts[col]) for col in x_values]
+
+		plt.plot(x_values, y_values, label=place)
+
+	label_figure(x_values, y_values, mode, filename)
+
+def label_figure(x_values, y_values, mode, filename):
 	days = len(x_values)
 	plt.title(f'Daily {mode}, Last {days} Days')
-	skip = max(len(x_values)//4, 1) # Helps ensure we don't add too many date tick marks
+	skip = max(len(x_values)//5, 1) # Helps ensure we don't add too many date tick marks
 	plt.xticks(x_values[::skip])
 	plt.xlabel("Date (MM/DD/YY)")
 	plt.ylabel(f"{mode}")
 	plt.legend()
-	plt.savefig(f'last_{days}_{mode}.png')
-	plt.show()
-
-
-def load_USA_time_series_data():
-	#URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
-	URL = 'time_series_covid19_confirmed_US.csv'
-	return pd.read_csv(URL)
-
-def load_USA_deaths():
-	URL = 'time_series_covid19_deaths_US.csv'
-	return pd.read_csv(URL)
-
-
-states = ['Massachusetts', 'New Hampshire']
-retrieve_state_data(states, 250, 'Cases')
+	filename = filename if filename else f'{mode}_last_{days}.png'
+	plt.savefig(filename)
+	plt.close()
