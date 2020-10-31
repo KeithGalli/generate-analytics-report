5cced5faa6eb60fbed2b992f4dad2c39b252f575
Keith Galli <k.galli603@gmail.com>
2020-10-31 16:38:53 -0400

    Refactored create_case_maps code

---
+++ create_case_maps.py
@@ -0,0 +1,73 @@
+# Python libraries
+import pandas as pd
+import plotly.express as px
+from datetime import datetime, timedelta
+
+# Local files
+from us_state_abbrev import us_state_abbrev
+from time_series_analysis import load_relevant_data
+
+
+yesterday = (datetime.today() - timedelta(days=1)).strftime("%m/%d/%y")
+# Uncomment below line for testing
+yesterday = "10/10/20"
+
+def plot_usa_case_map(filename=None):
+	df = load_relevant_data()
+	dates = list(df.columns)
+	df = df.groupby('Province_State')[dates].agg('sum')
+	create_usa_figure(df, filename)
+
+def plot_global_case_map(filename=None):
+	df = load_relevant_data(us_data=False)
+	dates = list(df.columns)
+	df = df.groupby('Country/Region')[dates].agg('sum')
+	create_global_figure(df, filename)
+
+def create_usa_figure(df, filename):
+
+	df['state'] = [us_state_abbrev.get(x, None) for x in list(df.index)]
+	df['Cases'] = df.diff(axis=1)[yesterday]
+
+	fig = px.choropleth(df,
+                    locations="state",
+                    locationmode="USA-states",
+                    scope="usa",
+                    color="Cases",
+                    hover_name="state",
+                    color_continuous_scale='Peach',
+                    title=f"US Daily Cases, {yesterday}",
+                    width=1000,
+                    #height=500,
+                    range_color=[0,3000])
+
+	fig.update_layout(margin=dict(l=0, r=0, t=70, b=0), title={"font": {"size": 40}, "x":0.5},)
+	# fig.show()
+	filename = filename if filename else "usa_chart.png"
+	fig.write_image(filename, engine='kaleido')
+
+def create_global_figure(df, filename):
+
+	df['Country'] = df.index
+	df['Cases'] = df.diff(axis=1)[yesterday]
+
+	fig = px.choropleth(df,
+                    locations="Country",
+                    locationmode="country names",
+                    scope="world", # Try 'europe', 'africa', 'asia', 'south america', 'north america'
+                    color="Cases",
+                    hover_name="Country",
+                    #projection="miller",
+                    color_continuous_scale='Peach',
+                    title=f"Global Daily Cases, {yesterday}",
+                    width=1000,
+                    #height=500,
+                    range_color=[0,50000])
+
+	fig.update_layout(margin=dict(l=0, r=0, t=70, b=0), title={"font": {"size": 40}, "x":0.5},)
+	# fig.show()
+	filename = filename if filename else "global_chart.png"
+	fig.write_image(filename, engine='kaleido')
+
+plot_usa_case_map()
+plot_global_case_map()
--- time_series_analysis.py
+++ time_series_analysis.py
@@ -17,7 +17,7 @@ def plot_countries(countries=['US'], days=7, mode=CASES, filename=None):
 def load_relevant_data(us_data=True, mode=CASES):
 	# This can be changed to your local directory (./) for testing purposes
 	#BASE_PATH = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
-	BASE_PATH = './'
+	BASE_PATH = './data/'
 	if us_data and mode == CASES:
 		PATH = BASE_PATH + 'time_series_covid19_confirmed_US.csv'
 	elif us_data and mode == DEATHS:
@@ -32,7 +32,7 @@ def load_relevant_data(us_data=True, mode=CASES):
 def plot_data(df, places, days, mode, column, filename):
 	for place in places:
 		cumulative_data = df[df[column] == place]
-		counts = cumulative_data.diff(axis=1)
+		counts = cumulative_data.diff(axis=1) # Converts from total case count to daily case count
 		x_values = list(counts.columns[-days:])
 		y_values = [int(counts[col]) for col in x_values]
 
