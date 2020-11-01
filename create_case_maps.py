c42a6def003ab9127cf8570fff6b6e99fa371a74
Keith Galli <k.galli603@gmail.com>
2020-11-01 15:14:11 -0500

    cleaning up code

--- create_case_maps.py
+++ create_case_maps.py
@@ -4,30 +4,26 @@ import plotly.express as px
 from datetime import datetime, timedelta
 
 # Local files
+from helper import load_relevant_data
 from us_state_abbrev import us_state_abbrev
-from time_series_analysis import load_relevant_data
 
-
-yesterday = (datetime.today() - timedelta(days=1)).strftime("%m/%d/%y")
-# Uncomment below line for testing
-#yesterday = "10/10/20"
-
-def plot_usa_case_map(filename=None):
+def plot_usa_case_map(filename=None, day=None):
 	df = load_relevant_data()
 	dates = list(df.columns)
 	df = df.groupby('Province_State')[dates].agg('sum')
-	create_usa_figure(df, filename)
+	create_usa_figure(df, filename, day)
 
-def plot_global_case_map(filename=None):
+def plot_global_case_map(filename=None, day=None):
 	df = load_relevant_data(us_data=False)
 	dates = list(df.columns)
 	df = df.groupby('Country/Region')[dates].agg('sum')
-	create_global_figure(df, filename)
+	create_global_figure(df, filename, day)
 
-def create_usa_figure(df, filename):
+def create_usa_figure(df, filename, day):
+	day = day if day else yesterday # default to yesterday's date if not provided
 
 	df['state'] = [us_state_abbrev.get(x, None) for x in list(df.index)]
-	df['Cases'] = df.diff(axis=1)[yesterday]
+	df['Cases'] = df.diff(axis=1)[day]
 
 	fig = px.choropleth(df,
                     locations="state",
@@ -36,20 +32,20 @@ def create_usa_figure(df, filename):
                     color="Cases",
                     hover_name="state",
                     color_continuous_scale='Peach',
-                    title=f"US Daily Cases, {yesterday}",
+                    title=f"US Daily Cases, {day}",
                     width=1000,
                     #height=500,
                     range_color=[0,3000])
 
 	fig.update_layout(margin=dict(l=0, r=0, t=70, b=0), title={"font": {"size": 20}, "x":0.5},)
-	# fig.show()
 	filename = filename if filename else "usa_chart.png"
 	fig.write_image(filename, engine='kaleido')
 
-def create_global_figure(df, filename):
+def create_global_figure(df, filename, day):
+	day = day if day else yesterday # default to yesterday's date if not provided
 
 	df['Country'] = df.index
-	df['Cases'] = df.diff(axis=1)[yesterday]
+	df['Cases'] = df.diff(axis=1)[day]
 
 	fig = px.choropleth(df,
                     locations="Country",
@@ -59,15 +55,19 @@ def create_global_figure(df, filename):
                     hover_name="Country",
                     #projection="miller",
                     color_continuous_scale='Peach',
-                    title=f"Global Daily Cases, {yesterday}",
+                    title=f"Global Daily Cases, {day}",
                     width=1000,
                     #height=500,
                     range_color=[0,50000])
 
 	fig.update_layout(margin=dict(l=0, r=0, t=70, b=20), title={"font": {"size": 20}, "x":0.5},)
-	# fig.show()
 	filename = filename if filename else "global_chart.png"
 	fig.write_image(filename, engine='kaleido')
 
-plot_usa_case_map()
-plot_global_case_map()
+if __name__ == '__main__':
+	yesterday = (datetime.today() - timedelta(days=1)).strftime("%m/%d/%y")
+	# Uncomment below line for testing
+	yesterday = "10/10/20"
+
+	plot_usa_case_map(day=yesterday) # saves as usa_chart.png by default
+	plot_global_case_map(day=yesterday) # saves as global_chart.png by default
